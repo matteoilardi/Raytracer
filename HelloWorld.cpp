79908947b5bfe0b65e06410a45b40de5591d41fe
Matteo Ilardi <matteoilardi1@gmail.com>
2025-02-26 14:28:50 +0100

    Aggiunta la possibilità (obbligo) di inserire il proprio nome

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
-int main(){
-	cout << "Hello world!" << endl;
+int main(int argv, char** argc){
+  	if(argv != 2){
+          cerr << "Uso del programma: inserisci anche il tuo nome!" << endl;
+          return 1;
+    }
+
+	cout << "Hello, "<<argc[1]<<"!" << endl;
 	return 0;
 
 }
