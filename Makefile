b1dc2822a42e0a31b45f0e3b895b5eacd4e5b12e
Matteo Ilardi <matteoilardi1@gmail.com>
2025-03-01 17:02:07 +0100

    Added CMake support

--- Makefile
+++
@@ -1,5 +0,0 @@
-HelloWorld.exe: HelloWorld.cpp
-	g++ HelloWorld.cpp -o HelloWorld.exe
-
-clean:
-	rm HelloWorld.exe
---
+++ src/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argv, char **argc) {
+  if (argv != 2) {
+    cerr << "Uso del programma: inserisci anche il tuo nome!" << endl;
+    return 1;
+  }
+
+  cout << "Hello, " << argc[1] << "!" << endl;
+  return 0;
+}
