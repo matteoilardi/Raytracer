605786c0b5c17a24500e0642e6f6357396326742
AndreaClini <andreaclini96@gmail.com>
2025-05-21 15:20:08 +0200

    minor fixes

--- include/random.hpp
+++ include/random.hpp
@@ -11,6 +11,7 @@
 
 #include <cstdint> // library for fixed size integer types
 #include <iostream>
+#include <cmath> // library for math functions
 
 // ------------------------------------------------------------------------------------------------------------
 // --------GLOBAL FUNCTIONS, CONSTANTS, FORWARD DECLARATIONS------------------
@@ -60,7 +61,7 @@ public:
   ///@brief generate random float uniformly distributed in [0, 1)
   float random_float() {
     uint32_t ran = random();
-    return static_cast<float>(ran) / pow(2.f, 32);
+    return static_cast<float>(ran) / std::pow(2.f,32);
   }
 
   ///@brief extract random numbers and discard them
